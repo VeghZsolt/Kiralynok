fe4f93a07dc95a8f96a493259a14694e5f1ca559
VeghZsolt <78847631+VeghZsolt@users.noreply.github.com>
2021-02-17 10:42:45 +0100

    6. feladat oop

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -2,9 +2,44 @@ package kiralynok;
 
 import java.io.IOException;
 
+
+
 public class Kiralynok {
+    
+    private Tabla tabla;
+
+    public Kiralynok()
+    {
+        tabla = new Tabla('#');
+        megoldasok();
+    }
+    
+    
     public static void main(String[] args) throws IOException {
+        Kiralynok kiir = new Kiralynok();
+        kiir.megoldasok();
         
+
+    }
+    
+    public void megoldasok()
+    {
+        feladat4();
+        feladat6();
+    }
+
+    private void feladat4()
+    {
+        System.out.println("4. feladat: üres tábla:");
+        Tabla tabla = new Tabla('#');
+        tabla.megjelenit();   
+    }
+
+    private void feladat6()
+    {
+        System.out.println("6. feladat feltoltott tabla:");
+        tabla.elhelyez(8);
+        tabla.megjelenit();
     }
     
 }
